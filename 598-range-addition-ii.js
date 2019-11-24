76496eb2d7eb92f7dd53919f94aa435aa2d172b3
Netwan97 <1193643266@qq.com>
2019-11-24 22:51:56 +0800

    add another  solution

--- 598-range-addition-ii.js
+++ 598-range-addition-ii.js
@@ -14,3 +14,30 @@ var maxCount = function(m, n, ops) {
     col.push(n);
     return row.sort((a,b) => a - b)[0] * col.sort((a,b) => a - b)[0];
 };
+
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function(m, n, ops) {
+    var len = ops.length;
+    if(len >= 1) {
+        var row = ops[0][0];
+        var col = ops[0][1];
+        for(var i = 0; i < ops.length - 1; ++i) {
+            var ops2 = ops[i + 1];
+            if(ops2[0] < row) {
+                row = ops2[0];
+            }
+            if(ops2[1] < col) {
+                col = ops2[1];
+            }
+        }
+    } else {
+        return m * n;
+    }
+    return Math.min(row, m) * Math.min(col,n);
+};
