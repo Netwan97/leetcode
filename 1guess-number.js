5a14ccb0063a188cb865a0d80748e6903a25933b
Netwan97 <1193643266@qq.com>
2019-11-28 22:43:46 +0800

    delete this two file

--- 1guess-number.js
+++
@@ -1,9 +0,0 @@
-var game = function(guess, answer) {
-    var count = 0;
-    for(var i = 0; i < 3; i++) {
-        if(guess[i] == answer[i]) {
-            count++;
-        }
-    }
-    return count;
-};
